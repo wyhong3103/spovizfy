2594c7986c184cd6d7d63ecd9fcdafa8a2b17333
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 16:27:17 +0800

    Add structure for Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,17 @@
-export const Home = () => {
+import '../styles/Home.css';
 
+export const Home = ({setPageState}) => {
+    return(
+        <div className="home-page">
+            <h1 className="home-title">
+                Spovizfy
+            </h1>
+            <p className="home-desc">
+                A way to experience your music with stunning visualizations that transform your favorite songs into immersive journeys.
+            </p>
+            <button className="view-result-btn">
+                Start 
+            </button>
+        </div>
+    )
 };
