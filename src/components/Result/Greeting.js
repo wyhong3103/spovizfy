8803380e4ae0f51135639b2ae70f4813fa460f55
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 18:21:07 +0800

    Add structure of Greeting

--- src/components/Result/Greeting.js
+++ src/components/Result/Greeting.js
@@ -1,10 +1,12 @@
+import '../../styles/Greeting.css';
+
 export const Greeting = ({data}) => {
     return(
         <div className="greeting-box">
-            <h1 className="greeting">Hi, {data.name}</h1>
+            <h1 className="greeting">Hi, <span className='green-span'>{data.name}</span></h1>
             <img src={data.pfp} alt="user-avatar" className="user-avatar"/>
             <p>
-                This is your Spovizfy report.
+                This is your <span className="green-span">Spovizfy</span> report.
             </p>
         </div>
     )
