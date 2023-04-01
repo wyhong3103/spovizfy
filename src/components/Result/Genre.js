f82e48e4c7fbc4e5d571b6c7c2333fecc803d61a
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 20:55:57 +0800

    Add structure to Genre

--- src/components/Result/Genre.js
+++ src/components/Result/Genre.js
@@ -1,3 +1,25 @@
-export const Genre = ({data, title}) => {
+import '../../styles/Genre.css';
 
+
+export const Genre = ({data, title}) => {
+    return(
+        <div className="genre-box">
+            <h1 className="genre-title">
+                {title}
+            </h1>
+            <div className="genre-list">
+                {
+                    data.map(
+                        (g) => {
+                            return(
+                                <div className="genre-tag">
+                                    {g}
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+        </div>
+    )
 };
