1f0c96b17dc564229934db3ef477bca0980b1579
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 19:22:49 +0800

    Add structure to Artist

--- src/components/Result/Artist.js
+++ src/components/Result/Artist.js
@@ -1,3 +1,41 @@
-export const Artist = ({data, title}) => {
+import '../../styles/Artist.css';
 
+export const Artist = ({data, title}) => {
+    return(
+        <div className="artist-box">
+            <h1 className="artist-box-title">
+                {title}
+            </h1>
+            <div className="artist-list">
+                {
+                    data.map(
+                        (item) => {
+                            return(
+                                <div className="artist-card">
+                                    <img src={item.img} alt="artist-avatar" className="artist-avatar"/>
+                                     
+                                    <h2 className="artist-name">
+                                        <a href={item.link} target="_blank" rel="noreferrer">{item.name}</a>
+                                    </h2>
+                                    <div className="artist-genre-box">
+                                        {
+                                            item.genre.map(
+                                                (g) => {
+                                                    return(
+                                                        <div className="genre-tag">
+                                                            {g}
+                                                        </div>
+                                                    )
+                                                }
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+        </div>
+    )
 };
