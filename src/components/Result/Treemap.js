7f5a7f4495f062967edec95638b06d69d785ec2f
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 20:56:15 +0800

    Add structure to Treemap

--- src/components/Result/Treemap.js
+++ src/components/Result/Treemap.js
@@ -1,3 +1,51 @@
-export const Treemap = ({data, title}) => {
+import { useEffect, useState } from "react";
+import "../../styles/Treemap.css";
+import ReactApexChart from "react-apexcharts";
 
+
+function getWindowSize() {
+  const {innerWidth, innerHeight} = window;
+  return {innerWidth, innerHeight};
+}
+
+export const Treemap = ({data, title}) => {
+    const [windowSize, setWindowSize] = useState(getWindowSize());
+    const series = [{data : data}];
+    const options = {
+        legend: {
+            show: false
+        },
+        chart: {
+            height: 350,
+            type: 'treemap',
+            toolbar : {
+                show : false
+            }
+        },
+        colors: [
+            "#1DB954"
+        ],
+    };
+
+    useEffect(() => {
+        function handleWindowResize() {
+        setWindowSize(getWindowSize());
+        }
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+        window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
+
+    return(
+        <div className="treemap-box">
+            <h1 className="treemap-box-title">
+                {title}
+            </h1>
+            <ReactApexChart series={series} options={options} type="treemap" height={windowSize.innerHeight * (7/10)} width={windowSize.innerWidth * (7/10)} />
+        </div>
+    )
 };
