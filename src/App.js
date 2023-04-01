8b90fe7ca9e6b3f8b9a3291143dbe4a5d66a9032
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 16:06:53 +0800

    Add home and result page to App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-export const App = () => {
+import { useState } from "react";
+import { Home } from "./pages/Home";
+import { Result } from "./pages/Result";
 
+export const App = () => {
+    
+    //0 = home page, 1 = result page
+    const [pageState, setPageState] = useState(0);
+
+    return(
+        <>
+            {
+                pageState === 0 ?
+                <Home/>
+                :
+                <Result/>
+            }
+        </>
+    )
 };
