3deb64d2db60bfd79b9913c78307f898720f5b21
wyhong3103 <wyhong3103@gmail.com>
2023-04-01 18:34:52 +0800

    Add basic structure of Song

--- src/components/Result/Song.js
+++ src/components/Result/Song.js
@@ -1,3 +1,30 @@
-export const Song = ({data, title}) => {
+import '../../styles/Song.css';
 
+export const Song = ({data, title}) => {
+    return (
+        <div className="song-box">
+            <h1 className="result-title">
+                {title}
+            </h1>
+            <div className="song-list">
+                {
+                    data.map(
+                        (item) => {
+                            return(
+                            <div className="song-grid">
+                                <img src={item.img} alt="song-img" className="song-img"/>
+                                <h2 className="song-name">
+                                    <a href={item.link} target="_blank" rel="noreferrer">{item.name}</a>
+                                </h2>
+                                <h3 className="song-artists"> 
+                                    {item.artists.join(', ')}
+                                </h3>
+                            </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+        </div>
+    )
 };
