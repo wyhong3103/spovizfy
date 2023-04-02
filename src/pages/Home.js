1502703d9feb13fd1f6caeef5e685b4b5818071f
wyhong3103 <wyhong3103@gmail.com>
2023-04-02 15:24:49 +0800

    Replace site with siteURL

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { apiHandler } from '../apiHandler';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { siteURL } from './siteUrl';
 import '../styles/Home.css';
 
 export const Home = ({setPageState}) => {
@@ -8,7 +9,7 @@ export const Home = ({setPageState}) => {
     //if valid, setPageState = 1,
     //loading screen, start fetching
     const [msg, setMsg] = useState("");
-    const grantURL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=http://localhost:3000/&response_type=token&scope=user-read-private%20user-top-read%20playlist-read-private%20`;
+    const grantURL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&redirect_uri=${siteURL}&response_type=token&scope=user-read-private%20user-top-read%20playlist-read-private%20`;
     const navigate = useNavigate();
 
     const login = () => {
