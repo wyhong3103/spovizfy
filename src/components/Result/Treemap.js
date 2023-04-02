cf025e64431f2c653e22123fc48bee5a4a3c0b76
wyhong3103 <wyhong3103@gmail.com>
2023-04-02 13:43:05 +0800

    Add responsiveness to Treemap

--- src/components/Result/Treemap.js
+++ src/components/Result/Treemap.js
@@ -1,9 +1,26 @@
+import { useState, useEffect } from "react";
 import { useElementOnSreen } from "../../hooks/elementOnScreen";
 import "../../styles/Treemap.css";
 import ReactApexChart from "react-apexcharts";
 
 export const Treemap = ({data, title}) => {
     const [containerRef, visible] = useElementOnSreen();
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        function handleResize() {
+            setWidth(window.innerWidth);
+            setHeight(window.innerHeight);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     const series = [{data : data}];
     const options = {
         legend: {
@@ -26,7 +43,36 @@ export const Treemap = ({data, title}) => {
             <h1 className="treemap-box-title">
                 {title}
             </h1>
-            <ReactApexChart series={series} options={options} type="treemap" height={500} width={1000} />
+            <ReactApexChart 
+                series={series} 
+                options={options} 
+                type="treemap" 
+                height={
+                    (
+                        width >= 1200 ? 
+                        500 
+                        : 
+                        (
+                            width >= 650 ? 
+                            800
+                            :
+                            1000
+                        )
+                    )
+                } 
+                width={
+                    (
+                        width >= 1200 ?
+                        1000
+                        : 
+                        (
+                            width >= 650 ? 
+                            500
+                            :
+                            300
+                        )
+                    )
+                } />
         </div>
     )
 };
